06c4b5d8dd5f65dd65bc67cf54e60228d367d652
Maia Oh <seoyeong.oh364@gmail.com>
2025-03-23 23:44:31 -0700

    Add JavaScript skillbuilder 1

---
+++ JavaScript/skillbuilder1/script.js
@@ -0,0 +1,15 @@
+// Morning Routine
+
+var isHappy = false;
+var happyDance = "💃 Let's get this day started! 💃";
+
+console.log("Starting morning...");
+console.log("Maia is happy?");
+console.log(isHappy);
+
+console.log("Drinking coffee...☕️");
+isHappy = true;
+
+console.log("Maia is happy?");
+console.log(isHappy);
+console.log(happyDance);
