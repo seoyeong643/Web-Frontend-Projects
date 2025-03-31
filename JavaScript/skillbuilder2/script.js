e706a861f3a224f8dc423121a401adb9971e11c3
Maia Oh <seoyeong.oh364@gmail.com>
2025-03-30 23:28:19 -0700

    Add JavaScript skillbuilder 2

---
+++ JavaScript/skillbuilder2/script.js
@@ -0,0 +1,11 @@
+var myVariable = "Maia";
+myVariable = "Dr. Small";
+console.log(myVariable);
+
+let myCat = "Blaze";
+const myZodiac = "Gemini";
+console.log(myCat);
+console.log(myZodiac);
+
+const STARTINGHP = 500;
+STARTINGHP = 300;
