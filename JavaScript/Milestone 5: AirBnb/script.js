590f9e1e85d7dab1d5b59dc3c19944191ed19c79
Maia Oh <seoyeong.oh364@gmail.com>
2025-04-06 19:18:14 -0700

    Add JS Milestone 5: AirBnb starter code

---
+++ JavaScript/Milestone 5: AirBnb/script.js
@@ -0,0 +1,12 @@
+function filterByCity(targetCity, listingCityArray) {}
+
+function filterByPrice(minPrice, maxPrice, listingPriceArray) {
+  // Note: Comment out the following line when you start working on this function!
+  return [...listingPriceArray.keys()];
+}
+
+// LevelUp!
+function filterByTypes(targetTypes, listingTypeArray) {
+  // Note: Comment out the following line when you attempt the LevelUp!
+  return [...listingTypeArray.keys()];
+}
