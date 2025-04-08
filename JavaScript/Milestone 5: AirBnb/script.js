2c5d92f0599bf5a7549b35de6dcead28aa6c9e58
Maia Oh <seoyeong.oh364@gmail.com>
2025-04-07 23:28:01 -0700

    Add JS Milestone 5: AirBnb finished code

--- JavaScript/Milestone 5: AirBnb/script.js
+++ JavaScript/Milestone 5: AirBnb/script.js
@@ -1,12 +1,38 @@
-function filterByCity(targetCity, listingCityArray) {}
+function filterByCity(targetCity, listingCityArray) {
+  const filteredArrayCity = [];
+
+  for (let i = 0; i < listingCityArray.length; i++) {
+    if (listingCityArray[i].toLowerCase() === targetCity.toLowerCase()) {
+      filteredArrayCity.push(i);
+    }
+  }
+
+  return filteredArrayCity;
+}
 
 function filterByPrice(minPrice, maxPrice, listingPriceArray) {
   // Note: Comment out the following line when you start working on this function!
-  return [...listingPriceArray.keys()];
+  const filteredArrayPrice = [];
+
+  for (let i = 0; i < listingPriceArray.length; i++) {
+    if (listingPriceArray[i] > minPrice && listingPriceArray[i] < maxPrice) {
+      filteredArrayPrice.push(i);
+    }
+  }
+
+  return filteredArrayPrice;
 }
 
 // LevelUp!
 function filterByTypes(targetTypes, listingTypeArray) {
   // Note: Comment out the following line when you attempt the LevelUp!
-  return [...listingTypeArray.keys()];
+  const filteredArrayTypes = [];
+
+  for (let i = 0; i < listingTypeArray.length; i++) {
+    if (targetTypes.includes(listingTypeArray[i])) {
+      filteredArrayTypes.push(i);
+    }
+  }
+
+  return filteredArrayTypes;
 }
