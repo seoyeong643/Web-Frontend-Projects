d17f9d9faa0af9a1bb7b89fec02d09053fed2e9d
Maia Oh <seoyeong.oh364@gmail.com>
2025-04-08 22:23:17 -0700

    Add JS Milestone 6: Dev Threadz finished code

--- JavaScript/Milestone 6: Dev Threadz/script.js
+++ JavaScript/Milestone 6: Dev Threadz/script.js
@@ -1,17 +1,33 @@
 function showAllProducts(products) {
   // forEach
+  products.forEach((product) => {
+    newProductCard(product);
+  });
 }
 
 function getFilteredProducts(productNames, query) {
   // filter
+  return productNames.filter((name) =>
+    name.toLowerCase().includes(query.toLowerCase())
+  );
 }
 
 function getConvertedPrices(productPrices, currency) {
   // map
+  const conversionRates = {
+    usd: 1,
+    gbp: 0.76, // 1 USD = 0.76 GBP
+    jpy: 110.57, // 1 USD = 110.57 JPY
+  };
+
+  const rate = conversionRates[currency.toLowerCase()] || 1;
+
+  return productPrices.map((price) => (price * rate).toFixed(2));
 }
 
 function getCartTotal(cartPrices) {
   // reduce
+  return cartPrices.reduce((total, price) => total + price, 0);
 }
 
 showAllProducts(PRODUCTS);
