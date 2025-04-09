ddb670ce603565ac19b4b6c8ca718aa51835ba5b
Maia Oh <seoyeong.oh364@gmail.com>
2025-04-08 22:22:09 -0700

    Add JS Milestone 6: Dev Threadz starter code

---
+++ JavaScript/Milestone 6: Dev Threadz/script.js
@@ -0,0 +1,17 @@
+function showAllProducts(products) {
+  // forEach
+}
+
+function getFilteredProducts(productNames, query) {
+  // filter
+}
+
+function getConvertedPrices(productPrices, currency) {
+  // map
+}
+
+function getCartTotal(cartPrices) {
+  // reduce
+}
+
+showAllProducts(PRODUCTS);
