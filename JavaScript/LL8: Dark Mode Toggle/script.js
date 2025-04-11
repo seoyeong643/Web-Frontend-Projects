ead0010757569629cc1042349e419e22158b399d
Maia Oh <seoyeong.oh364@gmail.com>
2025-04-10 19:34:51 -0700

    Add JS LL8: Dark Mode Toggle finished code

--- JavaScript/LL8: Dark Mode Toggle/script.js
+++ JavaScript/LL8: Dark Mode Toggle/script.js
@@ -1,3 +1,14 @@
-/* The following function will run every time the dark mode button is clicked. */
+/* The following function will run every time the dark mode button is clicked. Add logic to the function that will toggle the .dark class on and off of the <body> element. The text inside of the button should also change from "Dark Mode" to "Light Mode" based on which theme currently active. */
 
-function toggleDarkMode() {}
+function toggleDarkMode() {
+  const body = document.querySelector("body");
+  const darkModeButton = document.querySelector("#dark-mode-button");
+
+  body.classList.toggle("dark");
+
+  if (body.classList.contains("dark")) {
+    darkModeButton.innerText = "🌞 Light Mode";
+  } else {
+    darkModeButton.innerText = "🌛 Dark Mode";
+  }
+}
