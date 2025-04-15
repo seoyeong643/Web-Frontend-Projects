96f9b4b50ade726122aaf893d0745c975beae9a0
Maia Oh <seoyeong.oh364@gmail.com>
2025-04-14 23:49:04 -0700

    Add JS LL4: Functions from Scratch starter code

---
+++ JavaScript/LL4 Functions from Scratch/script.js
@@ -0,0 +1,18 @@
+// Basic function declaration syntax
+// Not super useful, probably didn't need a whole function for this....
+function helloWorld() {
+  console.log("Hello, World!");
+}
+helloWorld();
+// Function declaration with single parameter, ever so slightly more useful. It can log anything to the console! Why am I writing the function still, I could just console.log??
+function logMessage(message) {
+  console.log(message);
+}
+logMessage("HEllo, World!!");
+// Function with two parameters, which will preform a task that is more useful
+
+// Log total to produce an error and demonstrate scope
+// console.log(total);
+
+// Function with two parameters and a return statement
+// Much more useful
