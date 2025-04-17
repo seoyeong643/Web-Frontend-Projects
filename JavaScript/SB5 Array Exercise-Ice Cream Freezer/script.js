8356340a7b9754a5f29b629e33a95cf0d8b01272
Maia Oh <seoyeong.oh364@gmail.com>
2025-04-17 14:33:03 -0700

    Add JS SB5 Array Exercise-Ice Cream Freezer

---
+++ JavaScript/SB5 Array Exercise-Ice Cream Freezer/script.js
@@ -0,0 +1,29 @@
+// Do NOT modify these first two lines
+var flavors = [
+  "chocolate",
+  "vanilla",
+  "cookies and cream",
+  "rocky road",
+  "strawberry",
+];
+console.log(flavors);
+
+/* Complete the following tasks:
+(1) Add the flavor "root beer" to the end of the flavors array.
+(2) Log the new contents of the flavors array to the console.
+(3) Log "rocky road" to the console by accessing it through array indexing.
+(4) Log the number of items in the flavors array by using its length property. */
+
+// 👇 Your code below here 👇
+
+//Task 1
+flavors.push("root beer");
+
+//Task 2
+console.log(flavors);
+
+//Task 3
+console.log(flavors[3]);
+
+//Task 4
+console.log(flavors.length);
