aca427cdebe928dde6ecc3c65a19b283c792ec2e
Maia Oh <seoyeong.oh364@gmail.com>
2025-04-22 23:08:45 -0700

    Add JS LL5 Switch It Up starter code

---
+++ JavaScript/LL5 Switch It Up/script.js
@@ -0,0 +1,29 @@
+let zodiac = prompt("What is your zodiac sign?").toLowerCase();
+
+if (zodiac === "cancer") {
+  alert("You are a water sign!");
+} else if (zodiac === "scorpio") {
+  alert("You are a water sign!");
+} else if (zodiac === "pisces") {
+  alert("You are a water sign!");
+} else if (zodiac === "aries") {
+  alert("You are a fire sign!");
+} else if (zodiac === "leo") {
+  alert("You are a fire sign!");
+} else if (zodiac === "sagittarius") {
+  alert("You are a fire sign!");
+} else if (zodiac === "taurus") {
+  alert("You are an earth sign!");
+} else if (zodiac === "virgo") {
+  alert("You are an earth sign!");
+} else if (zodiac === "capricorn") {
+  alert("You are an earth sign!");
+} else if (zodiac === "gemini") {
+  alert("You are an air sign!");
+} else if (zodiac === "libra") {
+  alert("You are an air sign!");
+} else if (zodiac === "aquarius") {
+  alert("You are an air sign!");
+} else {
+  alert("Try again - that's not a valid zodiac sign!");
+}
