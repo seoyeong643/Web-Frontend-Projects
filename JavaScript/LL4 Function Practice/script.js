1765c4a65a4a45406f256cf1f5d65b77af5d1e2f
Maia Oh <seoyeong.oh364@gmail.com>
2025-04-22 22:52:12 -0700

    Add JS LL4 Function Practice starter code

---
+++ JavaScript/LL4 Function Practice/script.js
@@ -0,0 +1,64 @@
+// #1. capslock() - takes in one string and returns the same string with all capital letters
+function capslock(str) {
+  // 👇 Your code here 👇
+}
+// UNCOMMENT TO TEST:
+// console.log(capslock("javascript is bae"))
+
+console.log("--------------------------------------");
+
+// #2. dogYears() - takes in a number and returns that number multiplied by 7
+
+// 👇 Define the function here! 👇
+
+// UNCOMMENT TO TEST:
+// console.log(`You are ${dogYears(30)} in dog years!`)
+
+console.log("--------------------------------------");
+
+// #3. randomNum() - returns a random number between 1 and 10
+
+// HINT: use Math.random
+
+// HINT2: you'll need to mutiply the result by 10
+
+// LEVEL-UP: use Math.floor or Math.ceil to round the result to a whole number
+
+// 👇 Define the function here! 👇
+
+// UNCOMMENT TO TEST:
+// console.log(randomNum())
+// console.log(randomNum())
+// console.log(randomNum())
+
+console.log("--------------------------------------");
+
+// #4. asciiArt() - no parameters; prints out a piece of ascii art when run
+// Visit https://www.asciiart.eu/ for inspiration!
+
+// HINT: you can create multi-line strings if you use backticks instead of quotations marks (if there are backticks in the art itself, you'll need to replace those with single quotes aka apostrophes)
+
+// HINT #2: notice the instructions say to *print* the result (as opposed to return)
+
+// 👇 Define the function here! 👇
+
+// UNCOMMENT TO TEST:
+// asciiArt()
+
+console.log("--------------------------------------");
+
+// =============== BONUS ===============
+// whatMonthIsIt() - returns a number corresponding to the current month (July = 7)
+
+// HINT1: first look up how to get the current date from the Date javascript object
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/Date
+
+// HINT2: store the current date in a variable called "now", then call .getMonth() on that variable and return the result
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/getMonth
+
+// HINT3: If you find the numbers are off by one, make sure to adjust accordingly!
+
+// 👇 Define the function here! 👇
+
+// UNCOMMENT TO TEST:
+// console.log(whatMonthIsIt())
