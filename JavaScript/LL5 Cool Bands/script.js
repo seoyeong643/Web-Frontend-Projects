99faa6e7da3712910b38eb4195502bb973696c97
Maia Oh <seoyeong.oh364@gmail.com>
2025-04-22 22:58:35 -0700

    Add JS LL5 Cool Bands starter code

---
+++ JavaScript/LL5 Cool Bands/script.js
@@ -0,0 +1,15 @@
+// 1. Create an array of bands/artists (strings) named myBands. Be sure to use all lowercase letters.
+
+
+// 2a. Use the prompt() function to get the user's favorite band and store it in a variable called userChoice.
+// 2b. Convert the user's answer to lowercase.
+
+
+// 3a. Check the myBands array to see if it contains the user's answer.
+// 3b. If the user's band is not in the array, alert "Never heard of them!"
+// 3c. If the user's band is in the array, alert "OMG! I love them too!"
+// Hint: You'll need to research the indexOf() method and find out what it returns if the argument you supply isn't found in the target array.
+
+
+// 🧠 LevelUp:
+// If the user's band is not in the array, alert "I'll have to check them out!" and add the band to the array. Then console.log the array to confirm that the new item is present.
