c14899bb3c01aea841f84b8988880cfa36c6e40e
Maia Oh <seoyeong.oh364@gmail.com>
2025-04-24 18:12:20 -0700

    Add JS LL5 Switch It Up finished code

--- JavaScript/LL5 Switch It Up/script.js
+++ JavaScript/LL5 Switch It Up/script.js
@@ -1,29 +1,42 @@
 let zodiac = prompt("What is your zodiac sign?").toLowerCase();
 
-if (zodiac === "cancer") {
-  alert("You are a water sign!");
-} else if (zodiac === "scorpio") {
-  alert("You are a water sign!");
-} else if (zodiac === "pisces") {
-  alert("You are a water sign!");
-} else if (zodiac === "aries") {
-  alert("You are a fire sign!");
-} else if (zodiac === "leo") {
-  alert("You are a fire sign!");
-} else if (zodiac === "sagittarius") {
-  alert("You are a fire sign!");
-} else if (zodiac === "taurus") {
-  alert("You are an earth sign!");
-} else if (zodiac === "virgo") {
-  alert("You are an earth sign!");
-} else if (zodiac === "capricorn") {
-  alert("You are an earth sign!");
-} else if (zodiac === "gemini") {
-  alert("You are an air sign!");
-} else if (zodiac === "libra") {
-  alert("You are an air sign!");
-} else if (zodiac === "aquarius") {
-  alert("You are an air sign!");
-} else {
-  alert("Try again - that's not a valid zodiac sign!");
+switch (zodiac) {
+  case "cancer":
+    alert("You are a water sign!");
+    break;
+  case "scorpio":
+    alert("You are a water sign!");
+    break;
+  case "pisces":
+    alert("You are a water sign!");
+    break;
+  case "aries":
+    alert("You are a fire sign!");
+    break;
+  case "leo":
+    alert("You are a fire sign!");
+    break;
+  case "sagittarius":
+    alert("You are a fire sign!");
+    break;
+  case "taurus":
+    alert("You are an earth sign!");
+    break;
+  case "virgo":
+    alert("You are an earth sign!");
+    break;
+  case "capricorn":
+    alert("You are an earth sign!");
+    break;
+  case "gemini":
+    alert("You are an air sign!");
+    break;
+  case "libra":
+    alert("You are an air sign!");
+    break;
+  case "aquarius":
+    alert("You are an air sign!");
+    break;
+  default:
+    alert("Try again - that's not a valid zodiac sign!");
 }
