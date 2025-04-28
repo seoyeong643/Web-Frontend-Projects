bf39018316c7cd162e661fe37cac629c9f98c864
Maia Oh <seoyeong.oh364@gmail.com>
2025-04-27 17:03:30 -0700

    Add JS LL6: Lantana Alliance finished code

--- JavaScript/LL6: Lantana Alliance/script.js
+++ JavaScript/LL6: Lantana Alliance/script.js
@@ -1,25 +1,37 @@
 console.log(openStations);
 console.log(wishlist);
 
-// ☠️ CAREFUL - THIS WILL CRASH YOUR BROWSER
 // Should render all open stations to the page one by one
 // Should render the last items in the array first
 function addStations(stations) {
-  for (let i = 5; i < 0; i++) {
+  for (let i = stations.length - 1; i >= 0; i--) {
     addStationElement(stations[i]);
   }
 }
-// addStations(openStations)
+addStations(openStations);
 
 // Should render all wishlist items to the page one at a time
 function addWishlistItems(wishlist) {
-  // TODO
+  for (let i = 0; i < wishlist.length; i++) {
+    // use addWishlistElement to add a single wishlist item to the page
+    addWishlistElement(wishlist[i]);
+  }
 }
-// addWishlistItems(wishlist);
+addWishlistItems(wishlist);
 
 // Should return an array of station strings that include the user's query
 // This will automatically be called when a user clicks the search button
 function filterByCity(query, stations) {
+  // new array for filtered matches
   let matches = [];
-  // TODO
+  // loop through our stations
+  for (let i = 0; i < stations.length; i++) {
+    // check to see if current station contains the users query
+    if (stations[i].toLowerCase().includes(query.toLowerCase())) {
+      // add the station to the filtered matches
+      matches.push(stations[i]);
+    }
+  }
+  // return the filtered matches
+  return matches;
 }
