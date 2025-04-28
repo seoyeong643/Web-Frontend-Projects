a62deb00e7e92ca830eaa1183b7879b5d1fbeffb
Maia Oh <seoyeong.oh364@gmail.com>
2025-04-28 15:41:16 -0700

    Add JS SB4: Returns Vs Prints finished code

---
+++ JavaScript/SB4: Returns Vs Prints/script.js
@@ -0,0 +1,12 @@
+function printAThing() {
+  console.log("A thing.");
+}
+// printAThing();
+
+function returnAThing() {
+  return "A thing.";
+}
+
+// let result = returnAThing();
+// console.log(result);
+console.log(returnAThing());
