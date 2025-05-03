f39d16a0dcb05601ab56106f7da99658715e963a
Maia Oh <seoyeong.oh364@gmail.com>
2025-05-02 23:19:12 -0700

    Add JS SB5.4: If Else starter code

---
+++ JavaScript/SB5.4: If Else/script.js
@@ -0,0 +1,25 @@
+/* Add some conditional logic to the checkIfPoisonous() function below that checks if the animal passed to the function is poisonous. 
+
+If the animal is poisonous, console.log "You probably shouldn't pet that one..."
+If the animal is not poisonous, console.log "Give them a pet!"
+Once you've added your logic, call the function with the different animal variables provided to test it
+
+For some extra practice: 
+- update your conditional logic to check if the animal passed as an argument is poisonous OR vicious
+- update the logic to check if an animal is adorable. if so, console.log "Pet that one immediately!" */
+
+let kitty = "adorable";
+let pufferfish = "poisonous";
+let cockatiel = "vicious";
+let puppy = "adorable";
+let dartFrog = "poisonous";
+let blueRingedOctopus = "poisonous";
+let mountainLion = "vicious";
+let ant = "strong";
+let spottedOwl = "wise";
+
+function checkIfPoisonous(animal) {
+  // Your code here 👇
+}
+
+// checkIfPoisonous()
