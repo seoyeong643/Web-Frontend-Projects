f026dbc56998af147b93f72f8a66cadb41639ac4
Maia Oh <seoyeong.oh364@gmail.com>
2025-05-04 12:26:52 -0700

    Add JS SB5.6: Signup Form starter code

---
+++ JavaScript/SB5.6: Signup Form/script.js
@@ -0,0 +1,6 @@
+let passwordInput = document.getElementById("password-input");
+let confirmInput = document.getElementById("confirm-input");
+let minLengthWarning = document.getElementById("min-length");
+let maxLengthWarning = document.getElementById("max-length");
+let matchWarning = document.getElementById("match");
+let buttonElement = document.getElementsByTagName("button")[0];
