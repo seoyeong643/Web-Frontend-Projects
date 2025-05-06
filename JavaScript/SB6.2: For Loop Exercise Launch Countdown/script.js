7862a649bb105e5940d88f32f26bdf438f1df63a
Maia Oh <seoyeong.oh364@gmail.com>
2025-05-05 23:28:32 -0700

    Add JS SB6.2: For Loop Exercise Launch Countdown starter code

---
+++ JavaScript/SB6.2: For Loop Exercise Launch Countdown/script.js
@@ -0,0 +1,15 @@
+/* 
+	Using a single for loop, write a launch countdown that outputs the following:
+
+	30 seconds until launch.
+	20 seconds until launch.
+	10 seconds until launch.
+	5
+	4
+	3
+	2
+	1
+	Liftoff!
+
+	Hint: You’ll need to use if statements within your for loop to change the output based on the loop control variable.
+*/
