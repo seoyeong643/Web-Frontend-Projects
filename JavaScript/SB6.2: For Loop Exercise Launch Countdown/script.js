b739fc8d375d2805fefc22510a5058d5eb2138d9
Maia Oh <seoyeong.oh364@gmail.com>
2025-05-11 23:10:26 -0700

    Add JS SB6.2: For Loop Exercise Launch Countdown finished code

--- JavaScript/SB6.2: For Loop Exercise Launch Countdown/script.js
+++ JavaScript/SB6.2: For Loop Exercise Launch Countdown/script.js
@@ -13,3 +13,13 @@
 
 	Hint: You’ll need to use if statements within your for loop to change the output based on the loop control variable.
 */
+
+for (let i = 30; i >= 0; i--) {
+  if (i === 30 || i === 20 || i === 10) {
+    console.log(`${i} seconds until launch.`);
+  } else if (i <= 5 && i > 0) {
+    console.log(i);
+  } else if (i <= 5) {
+    console.log("Liftoff!");
+  }
+}
